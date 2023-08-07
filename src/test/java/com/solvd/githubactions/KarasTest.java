576295d333bb191686d60e7984eed16b80f6c562
solvd <solvd@solvds-MacBook-Pro-2.local>
2023-08-07 16:55:51 +0300

    yml was modified

--- src/test/java/com/solvd/githubactions/KarasTest.java
+++ src/test/java/com/solvd/githubactions/KarasTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 
 public class KarasTest extends BaseTest {
 
@@ -13,6 +12,7 @@ public class KarasTest extends BaseTest {
     public void goToMainPageTest() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setBrowserName("chrome");
+        System.out.println("GITURL" + System.getProperty("github.server_url"));
         driver = new RemoteWebDriver(capabilities);
         KarasMainPage karasMainPage = new KarasMainPage(driver);
         driver.quit();
