591aefffe8ee25868d6360e9588b47d3789a84dd
solvd <solvd@solvds-MacBook-Pro-2.local>
2023-08-07 16:36:25 +0300

    yml was modified

--- src/test/java/com/solvd/githubactions/KarasTest.java
+++ src/test/java/com/solvd/githubactions/KarasTest.java
@@ -13,7 +13,7 @@ public class KarasTest extends BaseTest {
     public void goToMainPageTest() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setBrowserName("chrome");
-        driver = new RemoteWebDriver(new URL("https://github.com/BelyukMihail/github-actions.git"), capabilities);
+        driver = new RemoteWebDriver(new URL("https://Mac-1691415195888.local:10.212.26.236"), capabilities);
         KarasMainPage karasMainPage = new KarasMainPage(driver);
         driver.quit();
 
