7168e55843bcd14c0f16dd9fa51ca57096560c13
solvd <solvd@solvds-MacBook-Pro-2.local>
2023-08-08 08:57:08 +0300

    yml was modified

--- src/test/java/com/solvd/githubactions/KarasTest.java
+++ src/test/java/com/solvd/githubactions/KarasTest.java
@@ -4,20 +4,31 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.util.Properties;
 
 public class KarasTest extends BaseTest {
 
     @Test
-    public void goToMainPageTest() throws MalformedURLException {
+    public void goToMainPageTest()  {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setBrowserName("chrome");
-        Properties properties = System.getProperties();
-        System.out.println("SYSTEM_PROPS: " + properties.getProperty("url"));
-        driver = new RemoteWebDriver(capabilities);
-        KarasMainPage karasMainPage = new KarasMainPage(driver);
-        driver.quit();
+        try (InputStream input = new FileInputStream("src/test/resources/system.properties")) {
+            Properties prop = new Properties();
+            prop.load(input);
+            System.out.println(prop.getProperty("db.password"));
+            System.out.println("SYSTEM_PROPS: " + prop.getProperty("url"));
+            driver = new RemoteWebDriver(capabilities);
+            KarasMainPage karasMainPage = new KarasMainPage(driver);
+            driver.quit();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
 
     }
 }
