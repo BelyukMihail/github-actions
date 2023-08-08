023bdea92c013d843ff5fc6b18f37501772e47ba
solvd <solvd@solvds-MacBook-Pro-2.local>
2023-08-08 10:52:43 +0300

    yml was modified

--- src/test/java/com/solvd/githubactions/KarasTest.java
+++ src/test/java/com/solvd/githubactions/KarasTest.java
@@ -13,9 +13,8 @@ public class KarasTest extends BaseTest {
     @Test
     public void goToMainPageTest() throws MalformedURLException {
         ChromeOptions options = new ChromeOptions();
-//        options.setCapability("cookieEnabled",true);
-        System.out.println("SYSTEM_PROPS: " + System.getenv("GITHUB_OUTPUT"));
-        driver = new RemoteWebDriver(new URL("https://github.com/BelyukMihail/github-actions/actions/runs/" + System.getenv().get("GITHUB_RUN_ID")), options);
+
+        driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), options);
         KarasMainPage karasMainPage = new KarasMainPage(driver);
         driver.quit();
     }
