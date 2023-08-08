586509acb793578cc49b8fc473da80fb227780f0
solvd <solvd@solvds-MacBook-Pro-2.local>
2023-08-08 10:04:56 +0300

    yml was modified

--- src/test/java/com/solvd/githubactions/KarasTest.java
+++ src/test/java/com/solvd/githubactions/KarasTest.java
@@ -1,5 +1,6 @@
 package com.solvd.githubactions;
 
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
@@ -11,12 +12,10 @@ public class KarasTest extends BaseTest {
 
     @Test
     public void goToMainPageTest() throws MalformedURLException {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setBrowserName("chrome");
-        capabilities.setCapability("cookieEnabled", true);
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("cookieEnabled",true);
         System.out.println("SYSTEM_PROPS: " + System.getenv().get("GITHUB_RUN_ID"));
-
-        driver = new RemoteWebDriver(new URL("https://github.com/BelyukMihail/github-actions/actions/runs/" + System.getenv().get("GITHUB_RUN_ID")), capabilities);
+        driver = new RemoteWebDriver(new URL("https://github.com/BelyukMihail/github-actions/actions/runs/" + System.getenv().get("GITHUB_RUN_ID")), options);
         KarasMainPage karasMainPage = new KarasMainPage(driver);
         driver.quit();
     }
