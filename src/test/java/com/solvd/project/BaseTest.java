c607c72ffb5e4f46f32aef0364d09a8a664c33d8
solvd <solvd@solvds-MacBook-Pro-2.local>
2023-08-09 09:18:05 +0300

    yml was changed

--- src/test/java/com/solvd/project/BaseTest.java
+++ src/test/java/com/solvd/project/BaseTest.java
@@ -21,8 +21,8 @@ public class BaseTest {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setBrowserName(browser);
         try {
-//            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
-            driver = new RemoteWebDriver(new URL(System.getProperty("selenium_url") + "/wd/hub"), capabilities);
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+//            driver = new RemoteWebDriver(new URL(System.getProperty("selenium_url") + "/wd/hub"), capabilities);
         } catch (MalformedURLException e) {
             throw new RuntimeException("Couldn't establish connection on provided URL.", e);
         }
